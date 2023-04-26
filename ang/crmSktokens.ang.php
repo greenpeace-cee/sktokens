3065c9f4cc248d99ae9032036913b1f2b22b388a
Jon Goldberg <jon@megaphonetech.com>
2023-04-26 15:06:41 -0400

    UI for editing labels

---
+++ ang/crmSktokens.ang.php
@@ -0,0 +1,28 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// \https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules/n
+return [
+  'js' => [
+    'ang/crmSktokens.js',
+    'ang/crmSktokens/*.js',
+    'ang/crmSktokens/*/*.js',
+  ],
+  'partials' => [
+    'ang/crmSktokens',
+  ],
+  'requires' => [
+    'crmSearchDisplay',
+    'crmUi',
+    'ui.bootstrap',
+    // 'crmUtil',
+    // 'ngRoute',
+  ],
+  'basePages' => ['civicrm/search', 'civicrm/admin/search'],
+  'bundles' => ['bootstrap3'],
+  'exports' => [
+    'crm-search-display-tokens' => 'E',
+  ],
+
+  'settings' => [],
+];
---
+++ ang/crmSktokens.js
@@ -0,0 +1,5 @@
+(function(angular, $, _) {
+  "use strict";
+  // Declare a list of dependencies.
+  angular.module('crmSktokens', CRM.angRequires('crmSktokens'));
+})(angular, CRM.$, CRM._);
